c3ec9b66853ae93bbb53b1e666254b2177a8ebbb
DanielMontanes2017 <edoras995@hotmail.com>
2017-01-23 20:33:21 +0100

    Correcció
    Error en una commanda

--- blinkk/blinkk.ino
+++ blinkk/blinkk.ino
@@ -15,7 +15,6 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 //*********************************** Loop ****************************************
-void loop()
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
